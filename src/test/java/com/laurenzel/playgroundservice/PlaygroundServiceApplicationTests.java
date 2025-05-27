cce4efc520160105f53cd11457fdabf14bde4b8f
Lauren Wenzel <laurenzel93@aol.com>
2025-05-26 20:21:23 -0400

    first commit

---
+++ src/test/java/com/laurenzel/playgroundservice/PlaygroundServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurenzel.playgroundservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
